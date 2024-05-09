40e2f9125e534ebc8d878c1334b9cc8f82b1317a
rekil49118@gmail.com <ilyaggit@gmail.com>
2024-05-09 17:25:13 +0400

    connect server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-const bool = true
+require('dotenv').config();
+
+const express = require('express')
+
+const PORT= 4321
+
+const sequelize = require('./db')
+
+const app = express()
+
+const start = async () =>{
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    }
+    catch(e){
+        console.error(e)
+    }
+}
+
+start().then(r => {})
