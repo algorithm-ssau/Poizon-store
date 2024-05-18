9f3a1ebbda4417a20ade33b9b7aa88eedcf121db
ilgorpy <rekil49118@gmail.com>
2024-05-19 01:50:59 +0400

    init models

---
+++ server/models/models.js
@@ -0,0 +1,82 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"}
+})
+
+const Basket = sequelize.define('basket', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Basket_Item = sequelize.define('basket_item', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Item = sequelize.define('item', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    rating: {type: DataTypes.DOUBLE, defaultValue: 0},
+    img: {type: DataTypes.STRING, allowNull: false}
+    
+})
+
+const Type = sequelize.define('type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}
+})
+
+const Brand = sequelize.define('brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}
+})
+
+const Item_Info = sequelize.define('item_info', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false}
+})
+
+const Rating = sequelize.define('rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.DOUBLE, unique: true, allowNull: false},
+})
+
+const Type_Brand = sequelize.define('type_brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+})
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(Basket_Item)
+Basket_Item.belongsTo(Basket)
+
+Item.hasOne(Basket_Item)
+Basket_Item.belongsTo(Item)
+
+Type.hasMany(Item)
+Item.belongsTo(Type)
+
+Brand.hasMany(Item)
+Item.belongsTo(Brand)
+
+Item.hasMany(Rating)
+Rating.belongsTo(Item)
+
+Item.hasMany(Item_Info)
+Item_Info.belongsTo(Item)
+
+Type.belongsToMany(Brand, {through: Type_Brand})
+Brand.belongsToMany(Type, {through: Type_Brand})
+
+module.exports = {
+    User, Basket, Basket_Item, Item, Item_Info, Type, Brand, Type_Brand, Rating
+}
