7444482a83e818c984955dbc56cd269d06e34460
ilgorpy <rekil49118@gmail.com>
2024-05-26 01:49:57 +0400

    add

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ const express = require('express')
 const sequelize = require('./db.js')
 const models = require('./models/models.js')
 const cors = require('cors')
-
+const router = require('./routes/index.js')
 
 const PORT = process.env.PORT || 4321
 
-
+const app = express()
+app.use(cors())
+app.use(express.json())
 app.use('/api', router)
 
 const start = async () => {
