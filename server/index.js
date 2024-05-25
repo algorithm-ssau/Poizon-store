be8d660f3b21fae6055f2e1bf4c44e2a8d92ceb7
slaboe <master05super05@gmail.com>
2024-05-25 18:53:42 +0400

    added routes

--- server/index.js
+++ server/index.js
@@ -4,10 +4,19 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db.js')
 const models = require('./models/models.js')
-
-const app = express()
+const cors = require('cors')
+const router = require('./routes/index.js')
 
 const PORT = process.env.PORT || 4321
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
+
+app.get('/', (req,res) => {
+    res.status(200).json({message : 'WORKING!!!'})
+})
 
 const start = async () => {
     try{
