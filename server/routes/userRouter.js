7c11af09f5db998122b3f94a2a47082cdd8161a6
slaboe <master05super05@gmail.com>
2024-05-26 18:49:55 +0400

    added Controller for User

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,12 @@
 const Router = require ('express')
 const router = new Router()
+const UserController = require ('../controllers/userController')
+const userController = require('../controllers/userController')
 
-router.post('/registration',)
-router.post('/login',)
-router.get('/auth', (req, res) => {
-    res.json({message: 'ALL WORKING'})
-})
+router.post('/registration', UserController.registration)
+router.post('/login',UserController.login)
+router.get('/auth', UserController.check )
+
 
 
 module.exports = router
