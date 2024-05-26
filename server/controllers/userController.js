7c65b1c29f43baccf736ef993fa616de30deeb19
slaboe <master05super05@gmail.com>
2024-05-26 18:49:55 +0400

    added Controller for User

---
+++ server/controllers/userController.js
@@ -0,0 +1,19 @@
+class UserController{
+    async registration(req, res)
+    {
+
+    }
+
+    async login(req, res)
+    {
+
+    }
+
+    async check(req, res)
+    {
+
+    }
+
+}
+
+module.exports = new UserController()
