5c74e1d2a532ff8fe0220858e5b5c9ed41ec99ba
ilgorpy <rekil49118@gmail.com>
2024-06-05 17:53:39 +0400

    added role

--- server/routes/itemRouter.js
+++ server/routes/itemRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController')
+const checkRoleMiddleware = require('../middleware/checkRoleMiddleware')
 
-router.post('/', itemController.create)
+router.post('/', checkRoleMiddleware('ADMIN'), itemController.create)
 router.get('/', itemController.getAll)
 router.get('/:id', itemController.getOne)
 
