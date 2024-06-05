2a4c6cedbb41da3787ef2a6bc6b74a5c732f2abf
ilgorpy <rekil49118@gmail.com>
2024-06-05 17:03:19 +0400

    add last get method

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,18 +1,27 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Item} = require('../models/models')
+const {Item, Item_Info} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class ItemController {
     async create(req, res, next){
         try{
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let filename = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', filename))
-    
+
             const item = await Item.create({name, price, brandId, typeId, img:filename})
-    
+
+            if (info){
+                info = JSON.parse(info)
+                info.array.forEach(element => 
+                    Item_Info.create({
+                        title: element.title,
+                        description: element.description,
+                        itemId: item.id
+                }));
+            }
             return res.json(item)
         }
         catch (e){
@@ -20,10 +29,32 @@ class ItemController {
         }
     }
     async getAll(req, res){
-        
+        let {brandId, typeId, limit, page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+        let items
+        if (!brandId && !typeId){
+            items = await Item.findAndCountAll({limit, offset})
+        }
+        if (brandId && !typeId){
+            items = await Item.findAndCountAll({where:{brandId}, limit, offset})
+        }
+        if (!brandId && typeId){
+            items = await Item.findAndCountAll({where:{typeId}, limit, offset})
+        }
+        if (brandId && typeId){
+            items = await Item.findAndCountAll({where:{brandId, typeId}, limit, offset})
+        }
+        return res.json(items)
     }
     async getOne(req, res){
-        
+        const {id} = req.params
+        const item = await Item.findOne(
+            {where: {id},
+            include: [{model: Item_Info, as: 'info'}]}
+        )
+        return res.json(item)
     }
 }
 
